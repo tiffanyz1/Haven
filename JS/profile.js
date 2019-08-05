9630a6b6698ab2cab71f1176fb3ac471019e87b9
strawberrycupcakes <52976794+strawberrycupcakes@users.noreply.github.com>
2019-08-05 14:19:50 -0400

    Add files via upload

---
+++ JS/profile.js
@@ -0,0 +1,9 @@
+function info(){
+  var contacts = document.getElementById("address").value;
+  var password = document.getElementById("contacts").value;
+  var password = document.getElementById("locations").value;
+  window.localStorage.setItem('Contacts',contacts)
+  window.localStorage.setItem('locations', locations)
+  window.localStorage.setItem('locations', address)
+
+}
