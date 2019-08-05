540f3bea25d4cc6c0e0b27a9568ac6ee237636c2
strawberrycupcakes <52976794+strawberrycupcakes@users.noreply.github.com>
2019-08-05 14:39:10 -0400

    Update signup.js

--- JS/signup.js
+++ JS/signup.js
@@ -3,5 +3,6 @@ function save(){
   var password = document.getElementById("password").value;
   window.localStorage.setItem('Email',email)
   window.localStorage.setItem('Password', password)
-
+  console.log(window.localStorage.getItem('Email'))
+  window.location.href = 'file:///C:/Users/Leila%20F/Documents/Haven/HTML/profile.html'
 }
