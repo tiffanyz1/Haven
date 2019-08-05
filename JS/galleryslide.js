794f17b907f301362ffe091ff412446905f7d906
tiffanyz1 <53485452+tiffanyz1@users.noreply.github.com>
2019-08-05 13:05:38 -0400

    Add files via upload

---
+++ JS/galleryslide.js
@@ -0,0 +1,30 @@
+
+
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("galleryslide");
+  if (n > slides.length) {slideIndex =0}
+  if (n < 0) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
