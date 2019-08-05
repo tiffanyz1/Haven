c2081ed8bb70acf48aa64119b2e4c00dab476959
strawberrycupcakes <52976794+strawberrycupcakes@users.noreply.github.com>
2019-08-05 11:11:13 -0400

    Add files via upload

---
+++ JS/signup.js
@@ -0,0 +1,7 @@
+function save(){
+  var email = document.getElementById("Email").value;
+  var password = document.getElementById("password").value;
+  window.localStorage.setItem('Email',email)
+  window.localStorage.setItem('Password', password)
+
+}
